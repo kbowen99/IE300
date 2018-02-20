4a96e2bf5d6780c3784659738b64ee63b466ad15
kbowen99 <kurtisbowen99@gmail.com>
2018-02-20 15:38:59 -0600

    Graph, seems incorrect

--- Case Study 1/main.py
+++ Case Study 1/main.py
@@ -1,13 +1,17 @@
 import csv
-from  matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
+from matplotlib import style
+style.use('ggplot')
 
 inFile = open('FlightTime.csv', 'r')
 outFile = open('results.txt', 'w')
 inReader = csv.reader(inFile, delimiter=',')
 allFlights = []
 uniqueCarriers = set()
-totalTime = 0
-totalDelays = 0
+avgCarrierDelay = []
+totalTime = 0.0
+totalDelays = 0.0
+
 
 def calcTFT(d, lori, ldes ):
     return .117 * d + .517 * (lori - ldes) + 20;
@@ -30,18 +34,56 @@ class Flight(object):
 for line in inReader:
     # Not Header & Greater than 230 min long (valid info)
     if line[0] != 'Flight Time' and line[5] != "" and line[7] != "" and line[9] > 230:
-        allFlights.append(Flight(fd=line[0], carrier=line[1], flightNum=line[2], origin=line[3], dest=line[4], depT=line[5], depD=line[6], arrT=line[7], arrD=line[8], ft=line[9]))
+        allFlights.append(Flight(fd=line[0], carrier=line[1], flightNum=line[2], origin=line[3], dest=line[4],
+                                 depT=line[5], depD=line[6], arrT=line[7], arrD=line[8], ft=line[9]))
         totalTime = totalTime + int(line[9])
         totalDelays = totalDelays + int(line[6]) + int(line[8])
-
         uniqueCarriers.add(line[1])
 inFile.close()
-
-totalDelays = 0
-for f in allFlights:
-    totalDelays = totalDelays + f.arrivalDelay + f.departureDelay
 avgDelay = totalDelays / len(allFlights)
 
 print str(len(allFlights)) + " Valid Observances"
 print "Target Flight Time: " + str(calcTFT(1741.16, -87.9, -118.41))
 print "Typical Time: " + str(calcTFT(1741.16, -87.9, -118.41) + avgDelay)
+
+outFile.write(str(len(allFlights)) + " Valid Observances \n")
+outFile.write("Target Flight Time: " + str(calcTFT(1741.16, -87.9, -118.41)) + "\n")
+outFile.write("Typical Time: " + str(calcTFT(1741.16, -87.9, -118.41) + avgDelay) + "\n")
+
+
+for carrier in uniqueCarriers:
+    carrierDelay = 0.0
+    numCarrierFlights = 0.0
+    for flight in allFlights:
+        if carrier in flight.carrier:
+            carrierDelay += flight.departureDelay + flight.arrivalDelay
+            numCarrierFlights += 1
+    avgCarrierDelay.append((carrierDelay / numCarrierFlights))
+
+    print carrier + " averaged a " + str(carrierDelay / numCarrierFlights) + " min delay with a total delay of " \
+          + str(carrierDelay) + " min across " + str(numCarrierFlights) + " flights"
+    outFile.write(carrier + " averaged a " + str(carrierDelay / numCarrierFlights) + " min delay with a total delay of " \
+          + str(carrierDelay) + " min across " + str(numCarrierFlights) + " flights" + "\n")
+
+
+minLength = avgCarrierDelay[0]
+minCarrier = list(uniqueCarriers)[0]
+for i in range(0, len(uniqueCarriers)):
+    if avgCarrierDelay[i] < minLength:
+        minLength = avgCarrierDelay[i]
+        minCarrier = list(uniqueCarriers)[i]
+
+print "Fastest Carrier: " + str(minCarrier)
+outFile.write("Fastest Carrier: " + str(minCarrier) + "\n")
+
+plt.bar(range(len(uniqueCarriers)), avgCarrierDelay, align='center')
+plt.xticks(range(len(uniqueCarriers)), uniqueCarriers)
+
+
+plt.title('Average Airliner Flight Delay')
+plt.ylabel('Avg. Delay')
+plt.xlabel('Airline Carrier')
+
+plt.show()
+
+print avgDelay
