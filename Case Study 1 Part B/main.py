3d06fc6f5c3970234bf606bcb70f4ad1806f99ff
kbowen99 <kurtisbowen99@gmail.com>
2018-03-13 15:52:29 -0500

    Code completed in class Still need to document

--- Case Study 1 Part B/main.py
+++ Case Study 1 Part B/main.py
@@ -1,7 +1,4 @@
 import csv
-from matplotlib import pyplot as plt
-from matplotlib import style
-style.use('ggplot')
 
 inFile = open('FlightDelay.csv', 'r')
 outFile = open('results.txt', 'w')
@@ -26,6 +23,13 @@ class Flight(object):
         self.delayed = isDelayed
 
 
+desiredDelays = list()
+desiredDelays.append(Flight(carr="AA", orig="JFK", dest="LAS"))
+desiredDelays.append(Flight(carr="B6", orig="JFK", dest="LAS"))
+desiredDelays.append(Flight(carr="VX", orig="SFO", dest="ORD"))
+desiredDelays.append(Flight(carr="WN", orig="SFO", dest="ORD"))
+
+
 for line in inReader:
     # Not Header & Greater than 230 min long (valid info)
     if line[0] != 'Carrier':
@@ -44,69 +48,77 @@ print str(len(allFlights)) + " Recorded Data Points."
 numDelays = 0.0
 for plane in allFlights:
     if plane.delayed:
-        numDelays += 1
+        numDelays += 1.0
 print str(numDelays) + " Recorded Delays."
 print "P(Y)=" + str(numDelays / len(allFlights)) + "; P(N)=" + str(1 - (numDelays / len(allFlights)))
 
 
 pYes = numDelays / len(allFlights)
-pNo= 1 - pYes
+pNo = 1.0 - pYes
 pOrigin = 1.0 / len(uniqueOrigins)
 pDestination = 1.0 / len(uniqueDestinations)
 pCarrier = 1.0 / len(uniqueCarriers)
 
 
-for airlineCarrier in uniqueCarriers:
-    for origin in uniqueOrigins:
-        for destination in uniqueDestinations:
-            numberWithOrigin = 0.0
-            numberWithOriginY = 0.0
+for wFlight in desiredDelays:
+    airlineCarrier = wFlight.carrier
+    origin = wFlight.origin
+    destination = wFlight.destination
+# for airlineCarrier in uniqueCarriers:
+#     for origin in uniqueOrigins:
+#         for destination in uniqueDestinations:
 
-            numberWithDestination = 0.0
-            numberWithDestinationY = 0.0
+    numberWithOrigin = 0.0
+    numberWithOriginY = 0.0
 
-            numberWithCarrier = 0.0
-            numberWithCarrierY = 0.0
+    numberWithDestination = 0.0
+    numberWithDestinationY = 0.0
 
-            for flight in allFlights:
-                if origin in flight.origin:
-                    numberWithOrigin += 1
-                    if flight.delayed:
-                        numberWithOriginY += 1
+    numberWithCarrier = 0.0
+    numberWithCarrierY = 0.0
 
-                if destination in flight.destination:
-                    numberWithDestination += 1
-                    if flight.delayed:
-                        numberWithDestinationY += 1
+    for flight in allFlights:
+        if origin in flight.origin:
+            numberWithOrigin += 1.0
+            if flight.delayed:
+                numberWithOriginY += 1.0
 
-                if airlineCarrier in flight.carrier:
-                    numberWithCarrier += 1
-                    if flight.delayed:
-                        numberWithCarrierY += 1
+        if destination in flight.destination:
+            numberWithDestination += 1.0
+            if flight.delayed:
+                numberWithDestinationY += 1.0
 
-            # Variable Scheme: probability of carrier I (y/n)
-            pCarrierIy = pGiven(n_hat=numberWithCarrierY, m=3.0, p=pCarrier, n=numberWithCarrier)
-            pCarrierIn = pGiven(n_hat=(numberWithCarrier - numberWithCarrierY), m=3.0, p=pCarrier, n=numberWithCarrier)
+        if airlineCarrier in flight.carrier:
+            numberWithCarrier += 1.0
+            if flight.delayed:
+                numberWithCarrierY += 1.0
 
-            pOriginIy = pGiven(n_hat=numberWithOriginY, m=3.0, p=pOrigin, n=numberWithOrigin)
-            pOriginIn = pGiven(n_hat=(numberWithOrigin - numberWithOriginY), m=3.0, p=pOrigin, n=numberWithOrigin)
+    # Variable Scheme: probability of carrier I (y/n)
+    pCarrierIy = pGiven(n_hat=numberWithCarrierY, p=pCarrier, n=numDelays)
+    pCarrierIn = pGiven(n_hat=(numberWithCarrier - numberWithCarrierY), p=pCarrier, n=(len(allFlights) - numDelays))
 
-            pDestIy = pGiven(n_hat=numberWithDestinationY, m=3.0, p=pDestination, n=numberWithDestination)
-            pDestIn = pGiven(n_hat=(numberWithDestination - numberWithDestinationY), m=3.0, p=pDestination, n=numberWithDestination)
+    pOriginIy = pGiven(n_hat=numberWithOriginY, p=pOrigin, n=numDelays)
+    pOriginIn = pGiven(n_hat=(numberWithOrigin - numberWithOriginY), p=pOrigin, n=(len(allFlights) - numDelays))
 
-            # print "Carrier:"
-            # print str(pCarrierIy) + "|" + str(pCarrierIn)
-            # print "Origin:"
-            # print str(pOriginIy) + "|" + str(pOriginIn)
-            # print "Destination:"
-            # print str(pDestIy) + "|" + str(pDestIn)
+    pDestIy = pGiven(n_hat=numberWithDestinationY, p=pDestination, n=numDelays)
+    pDestIn = pGiven(n_hat=(numberWithDestination - numberWithDestinationY), p=pDestination, n=(len(allFlights) - numDelays))
 
-            probDelay = pYes * pCarrierIy * pOriginIy * pDestIy
-            probNoDelay = pNo * pCarrierIn * pOriginIn * pDestIn
+    # print "Carrier:"
+    # print str(pCarrierIy) + "|" + str(pCarrierIn)
+    # print "Origin:"
+    # print str(pOriginIy) + "|" + str(pOriginIn)
+    # print "Destination:"
+    # print str(pDestIy) + "|" + str(pDestIn)
 
-            if probDelay >= probNoDelay:
-                print origin + "-" + destination + " on " + airlineCarrier + " Will be delayed."
-            else:
-                print origin + "-" + destination + " on " + airlineCarrier + " Will Not be delayed."
+    probDelay = pYes * pCarrierIy * pOriginIy * pDestIy
+    probNoDelay = pNo * pCarrierIn * pOriginIn * pDestIn
 
-            print "Probability of a delay: " + str(probDelay) + " Probability No Delay: " + str(probNoDelay)
+    if probDelay >= probNoDelay:
+        print origin + "-" + destination + " on " + airlineCarrier + " Will be delayed."
+        outFile.write(origin + "-" + destination + " on " + airlineCarrier + " Will be delayed. \n")
+    else:
+        print origin + "-" + destination + " on " + airlineCarrier + " Will Not be delayed."
+        outFile.write(origin + "-" + destination + " on " + airlineCarrier + " Will Not be delayed. \n")
+
+    print "Probability of a delay: " + str(probDelay) + " Probability No Delay: " + str(probNoDelay)
+    outFile.write("Probability of a delay: " + str(probDelay) + " Probability No Delay: " + str(probNoDelay) + "\n")
